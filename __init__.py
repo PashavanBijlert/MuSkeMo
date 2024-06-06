c374570d6812c0d98ba07266050b6f191e95ec0d
Pasha van Bijlert <pashavanbijlert@hotmail.com>
2024-06-06 16:03:01 +0200

    No change, debugging the automatic release action

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ bl_info = {
     "author_email" : "pasha.vanbijlert@naturalis.nl",
     "description" : "Build and visualize musculoskeletal models for use in 3rd party physics simulators",
     "blender" : (3, 0, 0),
-    "version" : (0, 2, 3),
+    "version" : (0, 2, 4),
     "location" : "",
     "warning" : "",
     "category" : "Physics",
