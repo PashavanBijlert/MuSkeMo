6df34b7b914429467140be14d3888c82f4fdc1ce
Pasha van Bijlert <pashavanbijlert@hotmail.com>
2025-01-18 21:50:05 +0100

    simple mus viz node now creates a sphere when a muscle only has one point. Prerequisite for #44

--- scripts/simple_muscle_viz_node.py
+++ scripts/simple_muscle_viz_node.py
@@ -9,7 +9,7 @@ def create_simple_muscle_node_group():
     group_output = muscle_node_group.nodes.new('NodeGroupOutput')
 
     # Set locations for Group Input and Output nodes
-    group_input.location = (-400, 0)
+    group_input.location = (-800, 0)
     group_output.location = (800, 0)
 
     # Create input sockets for the Curve and Radius directly in the node group
@@ -51,17 +51,96 @@ def create_simple_muscle_node_group():
     curve_length = muscle_node_group.nodes.new('GeometryNodeCurveLength')
     curve_length.location = (-200, -200)
 
+    # Domain size node (set to curve mode)
+    domain_size = muscle_node_group.nodes.new('GeometryNodeAttributeDomainSize')
+    domain_size.location = (-700, 150)
+    domain_size.component = 'CURVE'
+
+    # Compare node (set to integer, equal)
+    compare_node = muscle_node_group.nodes.new('FunctionNodeCompare')
+    compare_node.location = (-600, 150)
+    compare_node.data_type = 'INT'
+    compare_node.operation = 'EQUAL'
+    compare_node.inputs[3].default_value = 1
+
+    # Switch node (set to integer)
+    switch_node = muscle_node_group.nodes.new('GeometryNodeSwitch')
+    switch_node.location = (-400, 150)
+    switch_node.input_type = 'INT'
+    switch_node.inputs[2].default_value = 1
+
+    # Endpoint selection node
+    endpoint_selection = muscle_node_group.nodes.new('GeometryNodeCurveEndpointSelection')
+    endpoint_selection.location = (-150, 450)
+
+    # Integer node (set to 16)
+    integer_node = muscle_node_group.nodes.new('FunctionNodeInputInt')
+    integer_node.location = (-600, -200)
+    integer_node.integer = 16
+
+    # Math node (set to multiply, set second input to 2)
+    math_node = muscle_node_group.nodes.new('ShaderNodeMath')
+    math_node.location = (-250, 0)
+    math_node.operation = 'MULTIPLY'
+    math_node.inputs[1].default_value = 2
+
+    # UV Sphere node
+    uv_sphere = muscle_node_group.nodes.new('GeometryNodeMeshUVSphere')
+    uv_sphere.location = (-100, 100)
+
+    # Instances on points node
+    instances_on_points = muscle_node_group.nodes.new('GeometryNodeInstanceOnPoints')
+    instances_on_points.location = (0, 400)
+
+    # Realize instances node
+    realize_instances = muscle_node_group.nodes.new('GeometryNodeRealizeInstances')
+    realize_instances.location = (200, 200)
+
+    # Join geometry node
+    join_geometry = muscle_node_group.nodes.new('GeometryNodeJoinGeometry')
+    join_geometry.location = (400, 100)
+
+
+
 
     # Link the nodes: Group Input -> Curve to Mesh -> Merge by Distance -> Set Shade Smooth -> Group Output
     muscle_node_group.links.new(group_input.outputs['Curve'], named_attribute.inputs['Geometry']) 
+    muscle_node_group.links.new(group_input.outputs['Curve'], instances_on_points.inputs['Points']) 
+
+    muscle_node_group.links.new(group_input.outputs['Curve'], domain_size.inputs['Geometry']) 
+
+
+
     muscle_node_group.links.new(named_attribute.outputs['Geometry'],curve_to_mesh.inputs['Curve'])  # Curve link
     muscle_node_group.links.new(group_input.outputs['Curve'], curve_length.inputs['Curve'])
     muscle_node_group.links.new(curve_length.outputs['Length'], named_attribute.inputs['Value'])    #link the length into the named attribute so that we can call it outside the node
+    
     muscle_node_group.links.new(group_input.outputs['Radius'], curve_circle.inputs['Radius'])  # Radius link
+    muscle_node_group.links.new(group_input.outputs['Radius'], uv_sphere.inputs['Radius'])  # Radius link
+    
+
     muscle_node_group.links.new(curve_circle.outputs['Curve'], curve_to_mesh.inputs['Profile Curve'])
-    muscle_node_group.links.new(curve_to_mesh.outputs['Mesh'], merge_by_distance.inputs['Geometry'])  # Connect Curve to Mesh to Merge by Distance
+    muscle_node_group.links.new(curve_to_mesh.outputs['Mesh'], join_geometry.inputs['Geometry'])
+    muscle_node_group.links.new(realize_instances.outputs['Geometry'], join_geometry.inputs['Geometry'])
+                                
+    muscle_node_group.links.new(join_geometry.outputs['Geometry'], merge_by_distance.inputs['Geometry'])  # Connect join geometry to Merge by Distance
     muscle_node_group.links.new(merge_by_distance.outputs['Geometry'], set_shade_smooth.inputs['Geometry'])  # Connect Merge by Distance to Set Shade Smooth
     muscle_node_group.links.new(set_shade_smooth.outputs['Geometry'], group_output.inputs['Geometry'])  # Geometry link
+    
+    muscle_node_group.links.new(integer_node.outputs['Integer'], curve_circle.inputs['Resolution'])
+    muscle_node_group.links.new(integer_node.outputs['Integer'], uv_sphere.inputs['Rings'])
+    muscle_node_group.links.new(integer_node.outputs['Integer'], math_node.inputs[0])
+    muscle_node_group.links.new(math_node.outputs[0], uv_sphere.inputs['Segments'])
+
+    muscle_node_group.links.new(uv_sphere.outputs['Mesh'], instances_on_points.inputs['Instance'])
+    muscle_node_group.links.new(instances_on_points.outputs['Instances'], realize_instances.inputs['Geometry'])
+    muscle_node_group.links.new(uv_sphere.outputs['Mesh'], instances_on_points.inputs['Instance'])
+
+    muscle_node_group.links.new(domain_size.outputs['Point Count'], compare_node.inputs[2])
+    muscle_node_group.links.new(compare_node.outputs['Result'], switch_node.inputs['Switch'])
+    muscle_node_group.links.new(switch_node.outputs['Output'], endpoint_selection.inputs['Start Size'])
+    muscle_node_group.links.new(switch_node.outputs['Output'], endpoint_selection.inputs['End Size'])
+    muscle_node_group.links.new(endpoint_selection.outputs['Selection'], instances_on_points.inputs['Selection'])
 
     return
 
