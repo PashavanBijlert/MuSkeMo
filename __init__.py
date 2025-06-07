d6a30cf336508fc43bb0f6e6ba71979b3066160a
Pasha van Bijlert <80422565+PashavanBijlert@users.noreply.github.com>
2025-06-07 11:39:28 +0200

    version update

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ bl_info = {
     "author_email" : "pasha.vanbijlert@naturalis.nl",
     "description" : "Build and visualize musculoskeletal models for use in 3rd party physics simulators",
     "blender" : (4, 0, 0),
-    "version" : (0, 9, 7),
+    "version" : (0, 9, 75),
     "location" : "",
     "warning" : "",
     "category" : "Physics",
