6418977a13a4236e69745c6fb3e2c57a407fb964
Pasha van Bijlert <pashavanbijlert@hotmail.com>
2025-07-05 00:38:11 +0200

    Improved landmark panel behaviour. Addresses the crashing bug when attempting to add landmarks to landmarks #99. Landmarks are now always parented to MuSkeMo BODIES. If user selects a mesh to parent landmarks to, a parent body is created and selected mesh is turned into attached GEOMETRY.

--- scripts/landmark_marker_panel.py
+++ scripts/landmark_marker_panel.py
@@ -34,27 +34,80 @@ class CreateLandmarkOperator(Operator):
         active_obj = bpy.context.active_object  #should be the body that you want to parent the point to
         sel_obj = bpy.context.selected_objects  #should be the body that you want to parent the point to
 
+        sel_obj_backup = sel_obj.copy() #store this to restore the selected object state after the operator is finished
          #Ensure unique name
         if landmark_name in bpy.data.objects:
             self.report({'ERROR'}, "An object with the name '" + landmark_name + "' already exists in the scene. Choose a unique (unused) name for the new LANDMARK. Operation cancelled.")
             return {'FINISHED'}
+        
+        if not landmark_name:
+            self.report({'ERROR'}, "You didn't input a LANDMARK name. Choose a unique (unused) name for the new LANDMARK. Operation cancelled.")
+            return {'FINISHED'}
 
         if len(sel_obj)<1:
-            self.report({'ERROR'}, "Landmarks must be parented to a mesh, select the mesh you would like to parent the landmark to. Operation cancelled")
+            self.report({'ERROR'}, "Landmarks must be parented to a Mesh or a BODY, select one that you would like to parent the landmark to. Operation cancelled")
             return {'FINISHED'}
 
         if len(sel_obj)>1:
-            self.report({'ERROR'}, "Landmarks must be parented to a single mesh, select one mesh you would like to parent the landmark to. Operation cancelled")
+            self.report({'ERROR'}, "Landmarks must be parented to a single mesh or BODY, select one you would like to parent the landmark to. Operation cancelled")
             return {'FINISHED'}
         
-        target_mesh = sel_obj[0]
+        target_obj = sel_obj[0]
 
-        if target_mesh.type != 'MESH':
-            self.report({'ERROR'}, "Selected object '" + target_mesh.name + "' is not a MESH. Landmarks must be parented to meshes. Operation cancelled")
-            return {'FINISHED'}
+        create_parent_body = False #if we encounter a mesh without a parent body, this becomes true and we create one
+
+        if target_obj.get('MuSkeMo_type'): #if it has a MuSkeMo_type, and thus was created by MuSkeMo 
+
+            if target_obj['MuSkeMo_type'] not in ('BODY', 'GEOMETRY'): #if it's not a BODY or a GEOMETRY
+
+                self.report({'ERROR'}, "Selected object '" + target_obj.name + "' is not a BODY or a GEOMETRY (mesh attached to a body). Landmarks must be parented to BODIES or GEOMETRIES. Operation cancelled")
+                return {'FINISHED'}
+            
+            elif target_obj['MuSkeMo_type'] == 'GEOMETRY':
+                self.report({'WARNING'}, "The landmark was attached to the target mesh's parent body '" + target_obj.name + "_pbody'.")
+                
+                target_obj = target_obj.parent
 
 
 
+        else: #if it's not a MuSkeMo BODY, it can still get a landmark if it is a Mesh. If the Mesh has no parent body, we create a new body for this purpose.
+
+            if target_obj.type != 'MESH': #if it's not a mesh, throw an error and don't create the landmark
+
+                self.report({'ERROR'}, "Selected object '" + target_obj.name + "' is not a BODY or a mesh. Landmarks must be parented to BODIES or meshes. Operation cancelled")
+                return {'FINISHED'}
+            
+            elif not target_obj.parent: #if the mesh has no parent
+                create_parent_body = True
+                self.report({'WARNING'}, "A parent BODY was created for the selected mesh with the '" + target_obj.name + "_pbody' and placed at the origin. Landmarks will be attached to that newly created body")
+                
+            elif target_obj.parent.get('MuSkeMo_type')!= 'BODY': #if target mesh's parent object is not a  BODY, throw an error
+                self.report({'ERROR'}, "Selected mesh '" + target_obj.name + "' has a parent that is not a BODY. You must remove the mesh's parent before trying again. Operation cancelled")
+                return {'FINISHED'}
+            
+            else: #target meshes's parent is a MuSkeMo body, and we make that the target object.
+                self.report({'WARNING'}, "The landmark was attached to the target mesh's parent body '" + target_obj.name + "_pbody'.")
+                
+                target_obj = target_obj.parent
+
+        #if we trigger the creation of a parent body for an unparented mesh, we create a new body, and assign the target_obj
+        #as visual geometry, and make the parent body the new target_obj
+        if create_parent_body: 
+
+            from .create_body_func import create_body
+
+            pbodyname = target_obj.name + '_pbody'
+
+            create_body(name= pbodyname, size = muskemo.axes_size, self = self)
+
+            target_obj.select_set(True) #select the mesh
+            target_obj = bpy.data.objects[pbodyname] #set the parent body as the target_obj
+            target_obj.select_set(True) #select the parent body. Now a BODY and a mesh are selected
+            bpy.ops.body.attach_visual_geometry() #attach visual geometry
+
+
+
+        #### All of this should be moved over to a separate creation function
 
         target_loc = bpy.context.scene.cursor.location
         
@@ -72,14 +125,14 @@ class CreateLandmarkOperator(Operator):
 
         obj = bpy.data.objects[landmark_name]
         
-        obj.parent = target_mesh
+        obj.parent = target_obj
         
         #this undoes the transformation after parenting
-        obj.matrix_parent_inverse = target_mesh.matrix_world.inverted()
+        obj.matrix_parent_inverse = target_obj.matrix_world.inverted()
 
 
         #restore selection status
-        target_mesh.select_set(True)
+        target_obj.select_set(True)
                     
 
         ##### Assign a material
@@ -104,7 +157,8 @@ class CreateLandmarkOperator(Operator):
 
         muskemo.landmark_name = ''
 
-        
+        #Restore selection state
+        sel_obj_backup[0].select_set(True)
         return {'FINISHED'}
             
 
