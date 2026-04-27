61434192ae1646cc0439deea354af9477d2d8075
Pasha van Bijlert <pashavanbijlert@hotmail.com>
2026-04-27 22:21:30 +0200

    version increment.

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ bl_info = {
     "author_email" : "pasha.vanbijlert@naturalis.nl",
     "description" : "Build, visualize, and analyze musculoskeletal models",
     "blender" : (4, 0, 0),
-    "version" : (0, 9, 82),
+    "version" : (0, 9, 83),
     "location" : "",
     "warning" : "",
     "category" : "Physics",
